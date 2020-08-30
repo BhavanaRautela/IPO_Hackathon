6118a66c6edb6090cf3cf42956993c8b05c06b3c
BhavanaRautela <48850283+BhavanaRautela@users.noreply.github.com>
2020-08-30 18:03:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from datetime import time
-
 from flask_restful import Resource, Api
 from flask_httpauth import HTTPBasicAuth
 import win32security
@@ -41,42 +40,56 @@ def recognize_speech_from_mic(recognizer, microphone):
     return response
 
 
-
-
 app = Flask(__name__)
 # run_with_ngrok(app)
 api = Api(app, prefix="/api/v1")
 auth = HTTPBasicAuth()
 
-@auth.verify_password
-def verify(username,password):
-    username = "bhavanarautela"
-    password = "Password@123"
-    # query_parameters = request.args
-    # system_domain = query_parameters.get("Domain")
-    if not (username and password):
-        return False
-    else:
-        try:
-            sign_on_status = bool(win32security.LogonUser(username, "ktrainind", password,win32security.LOGON32_LOGON_NETWORK,win32security.LOGON32_PROVIDER_DEFAULT))
-        except:
-            sign_on_status=False
-        return sign_on_status
+
+@app.route('/', methods=['GET'])
+def hello():
+    ascii_banner = "Impact Players"
+    return ascii_banner
 
 
-class PrivateResource(Resource):
-    @auth.login_required
-    def get(self):
-        recognizer = sr.Recognizer()
-        microphone = sr.Microphone()
-        print("Say something: ")
-        time.sleep(5000)
-        guess = recognize_speech_from_mic(recognizer, microphone)
-        return guess["transcription"]
+@app.route('/transcript/', methods=['GET', 'POST'])
+def transcript():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+    print("Say something: ")
+    time.sleep(5000)
+    guess = recognize_speech_from_mic(recognizer, microphone)
+    return guess["transcription"]
+
+# @auth.verify_password
+# def verify(username,password):
+#     username = "bhavanarautela"
+#     password = "Password@123"
+#     # query_parameters = request.args
+#     # system_domain = query_parameters.get("Domain")
+#     if not (username and password):
+#         return False
+#     else:
+#         try:
+#             sign_on_status = bool(win32security.LogonUser(username, "ktrainind", password,win32security.LOGON32_LOGON_NETWORK,win32security.LOGON32_PROVIDER_DEFAULT))
+#         except:
+#             sign_on_status=False
+#         return sign_on_status
+
+#
+# class PrivateResource(Resource):
+#     #@auth.login_required
+#     def get(self):
+#         recognizer = sr.Recognizer()
+#         microphone = sr.Microphone()
+#         print("Say something: ")
+#         time.sleep(5000)
+#         guess = recognize_speech_from_mic(recognizer, microphone)
+#         return guess["transcription"]
 
 
-private = PrivateResource()
-api.add_resource(PrivateResource, '/private')
+# private = PrivateResource()
+# api.add_resource(PrivateResource, '/private')
 
 if __name__ == "__main__":
     app.run(debug=True)
